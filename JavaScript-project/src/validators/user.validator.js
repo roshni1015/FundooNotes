064844f73e1090bc692a438e3c264750f84a09a1
roshni1015 <roshniadatrao1110@gmail.com>
2022-07-19 12:45:17 +0530

    [Roshni]-User Registration

---
+++ JavaScript-project/src/validators/user.validator.js
@@ -0,0 +1,20 @@
+import Joi from '@hapi/joi';
+
+export const newUserValidator = (req, res, next) => {
+  const schema = Joi.object({
+    FirstName: Joi.string().min(4).required(),
+    LastName: Joi.string().min(4).required(),
+    EmailID: Joi.string().email().required(),
+    Password: Joi.string().min(8).required()
+  });
+  const { error, value } = schema.validate(req.body);
+  if (error) {
+    res.status(HttpStatus.BAD_REQUEST).json({
+      code: HttpStatus.BAD_REQUEST,
+      message: `${error}`,
+    });
+    next(error);
+  } else {
+    next();
+  }
+};
